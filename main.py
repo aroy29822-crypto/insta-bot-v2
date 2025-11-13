b82081c974730c5290a8eecff5ddc13e78bfe6ca
aroy29822-crypto <aroy29822@gmail.com>
2025-11-13 16:07:15 +0530

    docker instagram bot

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-from instagrapi import Client
 from flask import Flask
+from instagrapi import Client
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+app = Flask(__name__)
+
 USERNAME = os.getenv("IG_USERNAME")
 PASSWORD = os.getenv("IG_PASSWORD")
 
-app = Flask(__name__)
 cl = Client()
+cl.login(USERNAME, PASSWORD)
 
 @app.route("/")
 def home():
-    return "Insta bot running!"
+    return "Insta Bot Running!"
+
+@app.route("/send/<user>/<msg>")
+def send(user, msg):
+    cl.direct_send(msg, [user])
+    return f"Message sent to {user}"
 
 if __name__ == "__main__":
-    cl.login(USERNAME, PASSWORD)
     app.run(host="0.0.0.0", port=5000)
